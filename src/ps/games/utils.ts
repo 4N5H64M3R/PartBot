1a0f79ba4c1484a83ff6f522400cfae34d68a91c
PartMan <parth.mane@sprinklr.com>
2025-10-30 01:43:22 +0530

    feat: Filter out obscene autogenerated game IDs

--- src/ps/games/utils.ts
+++ src/ps/games/utils.ts
@@ -1,3 +1,5 @@
+import { RegExpMatcher, englishDataset, englishRecommendedTransformers } from 'obscenity';
+
 import { usePersistedCache } from '@/cache/persisted';
 import { isUGOActive } from '@/ps/ugo';
 
@@ -7,17 +9,29 @@ import type { UGOBoardGames } from '@/ps/ugo/constants';
 
 const idCache = usePersistedCache('gameId');
 
+const dataset = englishDataset.build();
+const obscenityMatcher = new RegExpMatcher({ ...dataset, ...englishRecommendedTransformers });
+
 // IDs are meant to be 4-character alphanumeric codes preceded with a '#'.
 // I'm assuming we won't need more than 36^4 IDs...
 export function generateId(): string {
 	if (process.env.NODE_ENV === 'development') return '#TEMP';
 
-	const lastId = idCache.get();
-	const newId = lastId + 1;
-	idCache.set(newId);
+	let lastId = idCache.get();
 
-	const idNum = (newId * 999979) % 36 ** 4;
-	return `#${idNum.toString(36).padStart(4, '0').toUpperCase()}`;
+	while (true) {
+		const newId = lastId + 1;
+		const idNum = (newId * 999979) % 36 ** 4;
+		const generatedId = idNum.toString(36).padStart(4, '0').toUpperCase();
+
+		if (!obscenityMatcher.hasMatch(generatedId)) {
+			idCache.set(newId);
+			return `#${generatedId}`;
+		}
+
+		// If obscene, try the next ID
+		lastId = newId;
+	}
 }
 
 export function createGrid<T>(x: number, y: number, fill: (x: number, y: number) => T) {
